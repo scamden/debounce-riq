ea70d075cdc0b3144c0d8b44779e18de8b3d7636
Sterling Camden <sterling@relateiq.com>
2015-05-15 12:10:33 -0700

    get tests passing

--- src/debounce.spec.js
+++ src/debounce.spec.js
@@ -1,28 +1,28 @@
-describe('debounce', function () {
-    var debounce = require('../debounce');
+describe('debounce', function() {
+    var debounce = require('./index.js');
 
-    it('should not call immediately', function () {
+    it('should not call immediately', function() {
         var spy = jasmine.createSpy();
         debounce(spy, 1)();
         expect(spy).not.toHaveBeenCalled();
     });
 
-    it('should call after the delay', function (done) {
+    it('should call after the delay', function(done) {
         var spy = jasmine.createSpy();
         debounce(spy, 1)();
-        setTimeout(function () {
+        setTimeout(function() {
             expect(spy).toHaveBeenCalled();
             done();
         }, 2);
     });
 
-    it('should not call if called again before delay', function (done) {
+    it('should not call if called again before delay', function(done) {
         var spy = jasmine.createSpy();
         var debounced = debounce(spy, 50000);
         debounced();
-        setTimeout(function () {
+        setTimeout(function() {
             debounced();
-            setTimeout(function () {
+            setTimeout(function() {
                 expect(spy).not.toHaveBeenCalled();
                 clearTimeout(debounce.timeout);
                 done();
@@ -30,23 +30,23 @@ describe('debounce', function () {
         }, 1);
     });
 
-    it('should only call once per time period', function (done) {
+    it('should only call once per time period', function(done) {
         var spy = jasmine.createSpy();
         var debounced = debounce(spy, 1);
         debounced();
         debounced();
-        setTimeout(function () {
+        setTimeout(function() {
             expect(spy.calls.count()).toBe(1);
             done();
         }, 2);
     });
 
-    it('should be possible to cancel it', function (done) {
+    it('should be possible to cancel it', function(done) {
         var spy = jasmine.createSpy();
         var debounced = debounce(spy, 1);
         debounced();
         clearTimeout(debounced.timeout);
-        setTimeout(function () {
+        setTimeout(function() {
             expect(spy).not.toHaveBeenCalled();
             done();
         }, 2);
